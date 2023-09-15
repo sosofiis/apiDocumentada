97b2f0ebdf3417ed771b4f68a781d81a6ddfe4ad
sosofiis <sofiaazleiria@gmail.com>
2023-09-15 18:10:05 -0300

    rotas http de crud

--- src/server.js
+++ src/server.js
@@ -12,3 +12,22 @@ const PORT = process.env.port || 3005;
 
 // Testar servidor
 app.listen(PORT, () => console.log(`Running at the port ${PORT}`))
+
+//Criando rota get
+app.get('/api/:id', (request, response) => {
+    response.send('Retorno de informações do banco de dados');
+});
+
+// Criando rota post
+app.post('/api/:id', (request, response) => {
+    response.send('Método utilizado para salvar informações');
+});
+
+// Criando rota put
+app.put('/api/:id', (request, response) => {
+    response.send('Método utilizado para editar informações');
+});
+
+app.delete('/api/:id', (request, response) => {
+    response.send('Método utilizado para deletar informações');
+});
